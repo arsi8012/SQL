85351c9eb02b89327666b479ceb0a6b7cc77f3f8
arsi8012 <olegkui@mail.ru>
2021-11-21 11:09:43 +0400

    Changed tests

--- src/test/java/ru/netology/data/DbUtilsData.java
+++ src/test/java/ru/netology/data/DbUtilsData.java
@@ -1,4 +1,4 @@
-package ru.netology.page;
+package ru.netology.data;
 
 import lombok.val;
 import org.apache.commons.dbutils.QueryRunner;
@@ -7,8 +7,8 @@ import org.apache.commons.dbutils.handlers.ScalarHandler;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DbUtilsPage {
-    public DbUtilsPage() {
+public class DbUtilsData {
+    private DbUtilsData() {
     }
 
     public static String getVerificationCode() {
@@ -30,7 +30,7 @@ public class DbUtilsPage {
         return verificationCode;
     }
 
-    public static String getStatusDb() {
+    public static String getStatusDb(String authInfo) {
         val statusSQL = "SELECT status FROM users WHERE login = ?;";
         val runner = new QueryRunner();
         String status = "blocked";
@@ -40,7 +40,7 @@ public class DbUtilsPage {
                         "jdbc:mysql://localhost:3306/app", "app", "pass"
                 );
         ) {
-            val statusDb = runner.query(conn, statusSQL, new ScalarHandler<>());
+            val statusDb = runner.query(conn, statusSQL, new ScalarHandler<>(), authInfo);
             status = (String) statusDb;
         } catch (SQLException e) {
             e.printStackTrace();
@@ -50,37 +50,9 @@ public class DbUtilsPage {
     }
 
 
-    public static void cleanCards() {
+    public static void cleanDatabase() {
         val delCards = "DELETE FROM cards;";
-        val runner = new QueryRunner();
-
-        try (
-                val conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/app", "app", "pass"
-                );
-        ) {
-            val cards = runner.update(conn, delCards, new ScalarHandler<>());
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-
-    public static void cleanAuthCodes() {
         val delAuthCodes = "DELETE FROM auth_codes;";
-        val runner = new QueryRunner();
-
-        try (
-                val conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/app", "app", "pass"
-                );
-        ) {
-            val authCodes = runner.update(conn, delAuthCodes, new ScalarHandler<>());
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-    }
-
-    public static void cleanUsers() {
         val delUsers = "DELETE FROM users;";
         val runner = new QueryRunner();
 
@@ -89,6 +61,8 @@ public class DbUtilsPage {
                         "jdbc:mysql://localhost:3306/app", "app", "pass"
                 );
         ) {
+            val cards = runner.update(conn, delCards, new ScalarHandler<>());
+            val authCodes = runner.update(conn, delAuthCodes, new ScalarHandler<>());
             val users = runner.update(conn, delUsers, new ScalarHandler<>());
         } catch (SQLException throwables) {
             throwables.printStackTrace();
--- src/test/java/ru/netology/test/TestDbInterection.java
+++ src/test/java/ru/netology/test/TestDbInterection.java
@@ -7,16 +7,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.DashboardPage;
-import ru.netology.page.DbUtilsPage;
+import ru.netology.data.DbUtilsData;
 import ru.netology.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDbInterection {
-    DbUtilsPage db = new DbUtilsPage();
-    private SelenideElement loginField;
-    private SelenideElement passwordField;
+//    private DbUtilsData db = new DbUtilsData();
+//    private SelenideElement loginField;
+//    private SelenideElement passwordField;
 
     @BeforeEach
     void setUp() {
@@ -28,7 +28,7 @@ public class TestDbInterection {
         val loginPage = new LoginPage();
         val authInfo = DataHelper.getAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
-        val verify = verificationPage.verify(DbUtilsPage.getVerificationCode());
+        val verify = verificationPage.verify(DbUtilsData.getVerificationCode());
         assertEquals("Личный кабинет", DashboardPage.getHeading());
     }
 
@@ -41,22 +41,13 @@ public class TestDbInterection {
         loginPage.invalidLogin(authInfo);
         loginPage.cleanFields();
         loginPage.invalidLogin(authInfo);
-        val status = DbUtilsPage.getStatusDb();
+        val status = DbUtilsData.getStatusDb("vasya");
         assertEquals("blocked", status);
     }
 
     @AfterAll
-    static void close() {
-        DbUtilsPage.cleanCards();
-    }
-
-    @AfterAll
-    static void close1() {
-        DbUtilsPage.cleanAuthCodes();
-    }
-
-    @AfterAll
-    static void close2() {
-        DbUtilsPage.cleanUsers();
+    static void close()
+    {
+        DbUtilsData.cleanDatabase();
     }
 }
